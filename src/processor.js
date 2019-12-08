bdaeac7cc3631a14be146a7c1065657e4c8de266
Eric Bratter <eric.bratter@gmail.com>
2019-12-08 08:41:29 -0500

    fixes to get it working and deployed

---
+++ src/processor.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = async (
+  data,
+  context,
+  appointmentRepository,
+  profileClient,
+  mailTransport
+) => {
+  const { params } = context;
+  const APP_NAME = 'Book It!';
+
+  const appointment = await appointmentRepository.findByAppointmentId(
+    params.appointmentId
+  );
+
+  const clientProfile = await profileClient.queryProfile(appointment.clientId);
+
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@bookit.com>`,
+    subject: 'Appointment Changed',
+    text: `Hey ${clientProfile.firstName}! There have been changes made to your appointment with ${appointment.businessName} on ${appointment.date}. Please log into the app and check.`,
+    to: clientProfile.email
+  };
+
+  await mailTransport.sendMail(mailOptions);
+};
